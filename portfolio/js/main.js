c28f184bff1f8efd14231ac2e692c7ce3a10bc8e
deke2444 <65369743+deke2444@users.noreply.github.com>
2020-06-11 11:15:21 +0900

    klusinn

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -88,8 +88,71 @@ function slideshow() {
 		slideshow();
 	}, 5000);
 }
-
 slideshow();
+
+function popupImage1() {
+  var popup = document.getElementById('js-popup1');
+  if(!popup) return;
+
+  var blackBg = document.getElementById('js-black-bg1');
+
+  var blackBg = document.getElementById('js-black-bg1');
+  var closeBtn = document.getElementById('js-close-btn1');
+  var showBtn = document.getElementById('js-show-popup1');
+
+  closePopUp(blackBg);
+  closePopUp(closeBtn);
+  closePopUp(showBtn);
+  function closePopUp(elem) {
+    if(!elem) return;
+    elem.addEventListener('click', function() {
+      popup.classList.toggle('is-show');
+    });
+  }
+}
+function popupImage2() {
+  var popup = document.getElementById('js-popup2');
+  if(!popup) return;
+
+  var blackBg = document.getElementById('js-black-bg2');
+
+  var blackBg = document.getElementById('js-black-bg2');
+  var closeBtn = document.getElementById('js-close-btn2');
+  var showBtn = document.getElementById('js-show-popup2');
+
+  closePopUp(blackBg);
+  closePopUp(closeBtn);
+  closePopUp(showBtn);
+  function closePopUp(elem) {
+    if(!elem) return;
+    elem.addEventListener('click', function() {
+      popup.classList.toggle('is-show');
+    });
+  }
+}
+function popupImage3() {
+  var popup = document.getElementById('js-popup3');
+  if(!popup) return;
+
+  var blackBg = document.getElementById('js-black-bg3');
+
+  var blackBg = document.getElementById('js-black-bg3');
+  var closeBtn = document.getElementById('js-close-btn3');
+  var showBtn = document.getElementById('js-show-popup3');
+
+  closePopUp(blackBg);
+  closePopUp(closeBtn);
+  closePopUp(showBtn);
+  function closePopUp(elem) {
+    if(!elem) return;
+    elem.addEventListener('click', function() {
+      popup.classList.toggle('is-show');
+    });
+  }
+}
+popupImage1();
+popupImage2();
+popupImage3();
 
 /* ----- option ----- */
 var id = ['type1','type2','type3','type4']; //指定するidを全て配列で渡す
